5d7c4ee9297d73b099515051ba1774ccd165fd79
xylanny <lqj04@outlook.com>
2026-09-19 00:05:22 +0800

    feat: 创建文档包，添加Github Actions配置而实现CI、CD（Pages）；注意添加base配置。

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "taro-ui",
+  base: "/taro-ui",
+  description: "One component lib created mainly by Vue3 + TS!",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
