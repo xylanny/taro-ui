742a9501f5bb57455b9af9b2aa27dc652c1495f2
xylanny <lqj04@outlook.com>
2026-09-19 18:16:29 +0800

    chore: 修改包名

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
           return assetInfo.name as string;
         },
 
-        advancedChunks: {
+        codeSplitting: {
           groups: chunkGroups,
         },
       },
