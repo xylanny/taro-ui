88851ca791261f73b778fc2b6c01163b0c0bbd7e
xylanny <lqj04@outlook.com>
2026-09-19 17:11:07 +0800

    chore: 按ES和UMD创建两个Vite构建配置，包括类型声明文件生成、ES分包等等；package.json中main字段、module字段与exports...import字段匹配，types字段与exports...types字段匹配，style字段与export./style.css匹配

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,14 +1,13 @@
 import "@taro-ui/theme/index.css";
 import components from "./components";
-import { createInstaller } from "@taro-ui/utils";
+import { createInstaller } from "../utils";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-// fas，“Font Awesome Solid”，即实心风格的整套图标
-// 把fas这一整套图标注册进全局library；注册后，就可以用字符串名字来引用图标。比如，<font-awesome-icon icon="user" />
 library.add(fas);
 const installer = createInstaller(components);
 
-export * from "@taro-ui/components"; // 按需引用
+// 类型产出时它们会被一起编译进 dist/types，相对引用才能在产物内部自解析
+export * from "../components";
 
-export default installer; // 全局注册
+export default installer;
